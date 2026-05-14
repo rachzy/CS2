d49ca1db771acf35b201562829cd6f477932b014
rachzy <henriqueferreira.fox@gmail.com>
2026-05-14 20:24:07 -0300

    change user running steamcmd

--- sniper/etc/entry.sh
+++ sniper/etc/entry.sh
@@ -195,7 +195,7 @@ while [[ $steamcmd_rc != 0 ]] && [[ $attempt -lt $MAX_ATTEMPTS ]]; do
         echo "Removing steamapps/appmanifest_730.acf..."
         rm -rf "${STEAMAPPDIR}/steamapps/appmanifest_730.acf"
     fi
-    eval gosu "${CONTAINER_USER}" bash "${STEAMCMDDIR}/steamcmd.sh" "${STEAMCMD_SPEW}"\
+    eval sudo bash "${STEAMCMDDIR}/steamcmd.sh" "${STEAMCMD_SPEW}"\
                                 +force_install_dir "${GAMEDIR}" \
                                 +@bClientTryRequestManifestWithoutCode 1 \
 			+login anonymous \
